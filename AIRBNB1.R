2336bcaf85364b363fc21ae964963e2fcebbf27c
vicky-marco <vicky_marco@hotmail.com>
2021-09-11 18:54:59 -0300

    1

---
+++ AIRBNB1.R
@@ -0,0 +1,13 @@
+#Análisis Airbnb
+
+#CARGO BASES
+
+library(tidyverse)
+
+DATA_AB2019 <- read.csv("https://query.data.world/s/v7xpthpx5kvhyccn2gy2vukmx47qnx", header=TRUE, stringsAsFactors=FALSE)
+
+
+
+
+
+
